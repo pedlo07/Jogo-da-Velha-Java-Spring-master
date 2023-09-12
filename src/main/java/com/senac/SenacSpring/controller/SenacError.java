53026010eca296307a59409c22187fd66e5acedf
pedlo07 <pedroalmeidaveber@gmail.com>
2023-09-12 20:30:47 -0300

    update to dunkelhai/Jogo-da-Velha-Java-Spring

---
+++ src/main/java/com/senac/SenacSpring/controller/SenacError.java
@@ -0,0 +1,16 @@
+package com.senac.SenacSpring.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SenacError implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(){
+        return "error";
+    }
+
+}
